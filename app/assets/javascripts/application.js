7ae8cacff074daa524053e41757b41fe6b75cafb
Diego Pinheiro <diego.pinheiro@jambu.com.br>
2018-11-08 14:08:32 -0300

    fist commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,35 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require bower_components/jquery/dist/jquery.min.js
+//= require bower_components/jquery-ui/jquery-ui.min.js
+//= require bower_components/bootstrap/dist/js/bootstrap.min.js
+//= require bower_components/raphael/raphael.min.js
+//= require bower_components/morris.js/morris.min.js
+//= require bower_components/jquery-sparkline/dist/jquery.sparkline.min.js
+//= require plugins/jvectormap/jquery-jvectormap-1.2.2.min.js
+//= require plugins/jvectormap/jquery-jvectormap-world-mill-en.js
+//= require bower_components/jquery-knob/dist/jquery.knob.min.js
+//= require bower_components/moment/min/moment.min.js
+//= require bower_components/bootstrap-daterangepicker/daterangepicker.js
+//= require bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js
+//= require plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js
+//= require bower_components/jquery-slimscroll/jquery.slimscroll.min.js
+//= require bower_components/fastclick/lib/fastclick.js
+//= require dist/js/adminlte.min.js
+//= require dist/js/pages/dashboard.js
+//= require dist/js/demo.js
+//= require plugins/iCheck/icheck.min.js
